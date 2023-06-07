2764afc62fa95211be175ce7407b8a1251a4e44a
Veysel Duran <vdrn76@gmail.com>
2023-06-07 15:21:49 +0300

    product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'finalproject';
   user: string = "Veysel Duran";
-  product1 = { productId: 1, productName: "Bardak", categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 1, productName: "Suluk", categoryId: 1, unitPrice: 5 }
-  product3 = { productId: 1, productName: "Yatak", categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 1, productName: "Cam", categoryId: 1, unitPrice: 5 }
-  product5 = { productId: 1, productName: "Sucuk", categoryId: 1, unitPrice: 5 }
-
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5]
+  
 }

d4ea3fa2440ef40f145fa7d1c414345af1ff460f
Veysel Duran <vdrn76@gmail.com>
2023-06-09 14:21:17 +0300

    angular pipes,toastr

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,10 +12,15 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText="";
+
+
+
 
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService:ToastrService
   ) {}
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -40,4 +46,10 @@ export class ProductComponent implements OnInit {
         this.dataLoaded = true;
       });
   }
+
+  addToCart(product:Product){
+    this.toastrService.success("sepete eklendi",product.productName)
+  }
+
+
 }

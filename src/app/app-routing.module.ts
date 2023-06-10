071b0df212f0bb621d9683449a873df44c220a3d
Veysel Duran <vdrn76@gmail.com>
2023-06-10 22:23:58 +0300

    secured,guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full",component:ProductComponent},
   {path:"products",component:ProductComponent},
   {path:"products/category/:categoryId",component:ProductComponent},
-  {path:"products/add",component:ProductAddComponent}
+  {path:"products/add",component:ProductAddComponent},
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
